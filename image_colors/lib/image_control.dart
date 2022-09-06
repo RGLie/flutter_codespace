d2c80f875e38ed9651267a79e55d567ff3faa7f2
Jaehyun Jeong <j2hyeon02@kaist.ac.kr>
2022-09-06 11:17:57 +0000

    import image picker package

---
+++ image_colors/lib/image_control.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageColor extends StatefulWidget {
+  const ImageColor({super.key});
+
+  @override
+  State<ImageColor> createState() => _ImageColorState();
+}
+
+class _ImageColorState extends State<ImageColor> {
+  late File imageFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Extract Colors in Image"),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              _getFromGallery();
+            },
+            child: Icon(Icons.image),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _getFromGallery() async {
+    PickedFile? pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+}
+
+
+
